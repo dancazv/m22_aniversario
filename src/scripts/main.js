c13ee742be387fc191587eff4b7afe1fc2e3833b
danzinazevedo <148352990+danzinazevedo@users.noreply.github.com>
2024-09-24 15:03:30 -0300

    corrigindo a data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Sep 12, 2024 19:00:00");
+const dataDoEvento = new Date("Sep 30, 2024 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
